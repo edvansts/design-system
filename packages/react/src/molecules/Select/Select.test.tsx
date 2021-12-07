4a96c978cbb6fa972e0637eca84c2946504b80af
Edvan de Matos <edvan.stt02@gmail.com>
2021-12-07 12:20:03 -0300

    feat: :white_check_mark: private repo

--- packages/react/src/molecules/Select/Select.test.tsx
+++ packages/react/src/molecules/Select/Select.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import Select from ".";
+import Select from "./Select";
 
 const options = [
   {
